ef2b377f6c6ac6632e7471ee77001b0ed05c5264
Christian Trolle Mikkelsen <Christian.H.Mikkelsen@gmail.com>
2014-01-31 13:22:29 +0100

    Initial commit

---
+++ scripts/dotnet.bat
@@ -0,0 +1,8 @@
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe', 'C:\Windows\Temp\dotNetFx40_Full_x86_x64.exe')" <NUL
+C:\Windows\Temp\dotNetFx40_Full_x86_x64.exe /q /norestart /repair /log C:\Windows\Temp\dotnet40.log
+
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://go.microsoft.com/fwlink/?LinkId=225702', 'C:\Windows\Temp\dotNetFx45_Full_x86_x64.exe')" <NUL
+C:\Windows\Temp\dotNetFx45_Full_x86_x64.exe /q /norestart /repair /log C:\Windows\Temp\dotnet45.log
+
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://go.microsoft.com/fwlink/?LinkId=322116', 'C:\Windows\Temp\NDP451-KB2858728-x86-x64-AllOS-ENU.exe')" <NUL
+C:\Windows\Temp\NDP451-KB2858728-x86-x64-AllOS-ENU.exe /q /norestart /repair /log C:\Windows\Temp\dotnet451.log
